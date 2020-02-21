76b29f98fa9f21c81d3f2ebf722a0a20de574df9
Iulian Eles <eiulian21@gmail.com>
2020-02-21 14:11:51 +0000

    changed uninstall.sh

--- scripts/uninstall.sh
+++ scripts/uninstall.sh
@@ -3,8 +3,8 @@
 #Script for uninstalling maldetect and Clamav
 
 #Remove Maldetect
+cd /
+echo "y" | /maldet*/files/uninstall.sh
 
-cd /maldet*/files/
-sh uninstall.sh
 #Remove Claav
 yum -y remove clamav
